1ede17e95b17e754bed5c03d7321342d0db2ff4f
Joshua Duplaa <joshuaduplaa@outlook.com>
2022-11-17 21:58:42 -0600

    Boiler plate code code complete

---
+++ simple/src/Components/Layout/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+//import NavBar from '../NavBar';
+//import ProjectSlides from '../ProjectSlides'
+
+const Layout = () => {
+    return <>
+        <p>Hello</p>
+    </>
+
+}
+
+export default Layout;
